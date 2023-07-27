7eb93729edba828c1ceb1313d5ca185c6cf77571
kieswetter-hub <50299190+kieswetter-hub@users.noreply.github.com>
2023-07-27 19:21:58 +0200

    else if statement added to return buzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -17,6 +17,8 @@ for (let i = 1; i <= userNumber; i++) {
     result = result + i;
     if (i % 3 === 0) {
         console.log("fizz");
+    } else if (i % 5 === 0) {
+        console.log("buzz")
     } else {
         console.log(i);
     }
