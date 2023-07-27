ff69c4ee129cd4b271de150499d20232800385c2
kieswetter-hub <50299190+kieswetter-hub@users.noreply.github.com>
2023-07-27 18:33:39 +0200

    startup html add pseudocode for java

---
+++ fizzbuzz.js
@@ -0,0 +1,9 @@
+/* fizzbuzz challenge */
+
+/* pseudocode
+ask user for numerical input
+create a loop printing 1 to user number
+if number is divisible by 3 print fizz
+if number is divisible by 5 print buzz
+if number is divisible by 3 and 5 print fizzbuzz
+*/
